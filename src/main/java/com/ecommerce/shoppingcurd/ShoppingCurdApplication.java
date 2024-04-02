3a8a054edd88e105dbb4108d1a8d7f00c0aabf78
Vimal Raveendran <vimalraveendra@gmail.com>
2024-04-02 22:35:21 +0200

    first commit

---
+++ src/main/java/com/ecommerce/shoppingcurd/ShoppingCurdApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shoppingcurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/shoppingcurd/ShoppingCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shoppingcurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
