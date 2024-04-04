295f894e3f9d79f39f0c6e8981ce8d9502a2417d
Vimal Raveendran <vimalraveendra@gmail.com>
2024-04-04 22:50:07 +0200

    added index.html

--- src/main/java/com/ecommerce/shoppingcurd/controller/ProductController.java
+++ src/main/java/com/ecommerce/shoppingcurd/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.ecommerce.shoppingcurd.controller;
 
 import com.ecommerce.shoppingcurd.entity.Product;
 import com.ecommerce.shoppingcurd.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -11,7 +13,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
   private ProductService productService;
+    @GetMapping("/")
     public String getProducts(Model model){
         List<Product> products = productService.getProducts();
         model.addAttribute("products",products);
